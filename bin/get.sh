676f04d5aae151ea9a8b0a8c6ac2735223730c6d
JamesRezo <james@rezo.net>
2020-07-04 19:03:21 +0200

    improve stats.json format

--- bin/get.sh
+++ bin/get.sh
@@ -6,22 +6,29 @@
 
 STATS_SPIP_NET="https://stats.spip.net/spip.php?page=stats.json"
 
-# Get Raw datas
-curl -s "${STATS_SPIP_NET}" | \
-    # Transform to have useful datas in correct type
-    jq '{
-        total_sites: .total_sites|tonumber,
-        spip: [.versions[]|{version, sites: .sites|tonumber}],
-        php: [.php[]|{version, sites: .sites|tonumber}]
-    }' > stats.json
+# Get SPIP Versions exposed
+curl -s "${STATS_SPIP_NET}" | jq '[.versions[]|.version]' > stats.json
 
-for v in $(jq -r '.spip[]|.version' stats.json);
+# For each version, get number of verified sites and number of PHP versions exposed
+TMP_FILES=
+for v in $(jq '.[]' stats.json);
 do
-    curl -s "${STATS_SPIP_NET}&v=${v}" | \
+    curl -s "${STATS_SPIP_NET}&v=${v//\"/}" | \
     jq '{
-        total_sites: .total_sites|tonumber,
+        version: '"${v}"',
+        sites: .total_sites|tonumber,
         php: [.php[]|{version, sites: .sites|tonumber}]
-    }' > "stats.${v}.json"
+    }' > "stats.${v//\"/}.json"
+    TMP_FILES="$TMP_FILES stats.${v//\"/}.json"
 done
+
+# Slurp temporary files into one json array
+echo "$TMP_FILES" | xargs jq -s '.' > stats.json
+echo "$TMP_FILES" | xargs rm
+
+echo -n "Verified sites:"
+jq '[.[]|.sites] | add' stats.json | xargs printf "% 6d\n"
+echo -n "PHP exposed   :"
+jq '[.[].php[]|.sites] | add' stats.json | xargs printf "% 6d\n"
 
 exit 0
