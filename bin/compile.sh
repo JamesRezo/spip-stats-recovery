515d113098d125a290081d7ecd2496001a045bec
JamesRezo <james@rezo.net>
2020-08-18 11:56:34 +0200

    Improve CSV Format for compile command (dates as ISO, version as string)

--- bin/compile.sh
+++ bin/compile.sh
@@ -8,8 +8,8 @@ echo "date,version,sites" > spip.csv
 
 for poll in $(find spip -type f -name \*.json | sort)
 do
-    datetime=$(TZ=":GMT" date -r "$poll" '+%Y-%m-%d %H:%M:%S')
-    for data in $(jq -r '.[]|[.version,.sites]|join(",")' "$poll")
+    datetime=$(TZ=":GMT" date -r "$poll" '+%Y-%m-%dT%H:%M:%SZ')
+    for data in $(jq -r '.[]|["\""+.version+"\"",.sites]|join(",")' "$poll")
     do
         echo "${datetime},${data}" >> spip.csv
     done
@@ -19,8 +19,8 @@ echo "date,version,sites" > php.csv
 
 for poll in $(find php -type f -name \*.json | sort)
 do
-    datetime=$(TZ=":GMT" date -r "$poll" '+%Y-%m-%d %H:%M:%S')
-    for data in $(jq -r '.[]|[.version,.sites]|join(",")' "$poll")
+    datetime=$(TZ=":GMT" date -r "$poll" '+%Y-%m-%dT%H:%M:%SZ')
+    for data in $(jq -r '.[]|["\""+.version+"\"",.sites]|join(",")' "$poll")
     do
         echo "${datetime},${data}" >> php.csv
     done
