2816ad6028ea1a4ac6c646a7b9818e1ca68ab4f5
JamesRezo <james@rezo.net>
2025-04-12 20:45:35 +0200

    feat(save): git config hardened

--- bin/save.sh
+++ bin/save.sh
@@ -12,8 +12,16 @@ source tools
 SPIP_JSON=${1:-spip.json}
 DATE=$(date -r "$SPIP_JSON" '+%Y/%m/%d/%H%M%S')
 
-[[ $DEBUG ]] && git status
-[[ $DEBUG ]] && echo "Committting with message \"poll ${DATE}\""
+[[ $DEBUG ]] && {
+    git status
+    echo "Committting with message \"poll ${DATE}\""
+    echo "user.email=${GIT_AUTHOR_EMAIL}"
+    echo "user.name=${GIT_AUTHOR_NAME}"
+}
+
+test -n "${GIT_AUTHOR_EMAIL}" && git config user.email "${GIT_AUTHOR_EMAIL}"
+test -n "${GIT_AUTHOR_NAME}" && git config user.name "${GIT_AUTHOR_NAME}"
+
 git add .
 git commit -m "poll ${DATE}"
 
