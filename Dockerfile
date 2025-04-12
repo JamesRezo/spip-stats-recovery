d19a0a260a5cc014170d9a245c488ff828274b9c
JamesRezo <james@rezo.net>
2025-04-12 17:54:44 +0200

    feat(compile): faster

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM alpine:3.21
 
 RUN apk add --no-cache bash make curl jq git && \
-    adduser --home /build --shell /bin/ash --disabled-password poller poller
+    adduser --home /home/poller --shell /bin/bash --disabled-password poller poller
 
 COPY bin/tools.sh /usr/local/bin/tools
 COPY bin/poll.sh /usr/local/bin/poll
@@ -10,8 +10,8 @@ COPY bin/print.sh /usr/local/bin/print
 COPY bin/compile.sh /usr/local/bin/compile
 COPY bin/save.sh /usr/local/bin/save
 COPY bin/Makefile /Makefile
-COPY .gitconfig /root
-COPY .gitignore_global /root
+COPY .gitconfig /home/poller
+COPY .gitignore_global /home/poller
 
 ENTRYPOINT [ "make", "-f", "/Makefile"]
 
--- bin/compile.sh
+++ bin/compile.sh
@@ -4,37 +4,55 @@
 # Description : Transform JSON stats files into CSV files, date are stored in GMT Timezone
 # Needs jq
 
-# TODO do not regenerate csv if exist and add what is missing ...
-echo "date,version,sites" > spip.csv
+SINCE=${1:-2022-02-23}
 
-for poll in $(find spip -type f -name \*.json | sort)
+# Lat commit formatted date
+LAST_COMMIT="$(git log -1 --format="%at" | xargs -I{} date -d @{} +%Y%m%d%H%M.%S)"
+
+[ -f spip.csv ] && {
+    touch -m -t "${LAST_COMMIT}" spip.csv
+    SPIP_POLL="$(find spip -type f -name \*.json -newer spip.csv | sort)"
+}
+[ -f php.csv ] && {
+    touch -m -t "${LAST_COMMIT}" php.csv
+    PHP_POLL="$(find php -type f -name \*.json -newer php.csv | sort)"
+}
+
+[ ! -f spip.csv ] && {
+    echo "date,version,sites" > spip.csv
+    SPIP_POLL="$(find spip -type f -name \*.json | sort)"
+}
+[ ! -f php.csv ] && {
+    echo "date,version,sites" > php.csv
+    PHP_POLL="$(find php -type f -name \*.json | sort)"
+}
+
+for poll in $SPIP_POLL
 do
-    datetime=$(TZ=":GMT" date -r "$poll" '+%Y-%m-%dT%H:%M:%SZ')
-    for data in $(jq -r '.[]|["\""+.version+"\"",.sites]|join(",")' "$poll")
-    do
-        echo "${datetime},${data}" >> spip.csv
-    done
-done
+    datetime="$(sed -e 's#^\w\+/##' -e 's#\.json$##' -e 's#/#-#g' -e 's#-\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)$#T\1:\2:\3Z#' <<< "${poll}")"
+    jq --arg datetime "${datetime}" -r '.[]|[$datetime,"\""+.version+"\"",.sites]|join(",")' "$poll"
+done >> spip.csv
 
-echo "date,version,sites" > php.csv
-
-for poll in $(find php -type f -name \*.json | sort)
+for poll in $PHP_POLL
 do
-    datetime=$(TZ=":GMT" date -r "$poll" '+%Y-%m-%dT%H:%M:%SZ')
-    for data in $(jq -r '.[]|["\""+.version+"\"",.sites]|join(",")' "$poll")
-    do
-        echo "${datetime},${data}" >> php.csv
-    done
-done
+    datetime="$(sed -e 's#^\w\+/##' -e 's#\.json$##' -e 's#/#-#g' -e 's#-\([0-9]\{2\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)$#T\1:\2:\3Z#' <<< "${poll}")"
+    jq --arg datetime "${datetime}" -r '.[]|[$datetime,"\""+.version+"\"",.sites]|join(",")' "$poll"
+done >> php.csv
 
-echo "date,version,sites" > spip.since-2022-02-23.csv && \
-tail -$(expr $(
-    expr $(wc -l spip.csv | tr "s" "," | cut -d, -f1 | tr -d "[:space:]") - $(grep -n 2022-02-23 spip.csv | head -1 | cut -d: -f1)
-) + 1) spip.csv >> spip.since-2022-02-23.csv
+FIRST="$(grep -n "${SINCE}" spip.csv | head -1 | cut -d: -f1)"
+[ "${FIRST}" != "" ] && {
+    LINES="$(wc -l spip.csv | cut -d' ' -f1)"
+    LAST="$(("${LINES}"-"${FIRST}"+1))"
+    echo "date,version,sites"
+    tail -n "${LAST}" spip.csv
+} > "spip.since-${SINCE}.csv"
 
-echo "date,version,sites" > php.since-2022-02-23.csv && \
-tail -$(expr $(
-    expr $(wc -l php.csv | tr "p" "," | cut -d, -f1 | tr -d "[:space:]") - $(grep -n 2022-02-23 php.csv | head -1 | cut -d: -f1)
-) + 1) php.csv >> php.since-2022-02-23.csv
+FIRST="$(grep -n "${SINCE}" php.csv | head -1 | cut -d: -f1)"
+[ "${FIRST}" != "" ] && {
+    LINES="$(wc -l php.csv | cut -d' ' -f1)"
+    LAST="$(("${LINES}"-"${FIRST}"+1))"
+    echo "date,version,sites"
+    tail -n "${LAST}" php.csv
+} > "php.since-${SINCE}.csv"
 
 exit 0
